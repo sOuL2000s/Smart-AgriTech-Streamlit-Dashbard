e81481f2938a3600844004b844638cbb61022089
sOuL2000s <souparnopaulreborn@gmail.com>
2025-07-13 02:57:49 +0530

    gunicorn run

--- app.py
+++ app.py
@@ -990,4 +990,5 @@ if __name__ == '__main__':
     sensor_inserter_thread.start()
 
     # Run the Flask app
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
